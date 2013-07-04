1ea06ddcc02dde375a7ff359d387c6c2b72c3fd1
Michael Myers <blackavr@gmail.com>
2013-07-04 12:42:55 -0700

    general case of the filename generator

--- lib/metar_grabber.rb
+++ lib/metar_grabber.rb
@@ -1,9 +1,9 @@
 class MetarGrabber
   module MetarGrabber::Filepath
-    def self.surface_filename(input_time)
+    def self.gem_filename(input_time,ext)
       input_time.utc
       date_part = input_time.gmtime.strftime('%Y%m%d')
-      "#{date_part}_sao.gem"
+      "#{date_part}_#{ext}.gem"
     end
 
   end
--- spec/filename_format_spec.rb
+++ spec/filename_format_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe MetarGrabber::Filepath do
-  describe '.surface_filename when UTC is same day' do
-    test_edt = MetarGrabber::Filepath.surface_filename(Time.new(2007,11,1,12, 5,0, "-05:00"))
+  describe '.gem_filename when UTC is same day' do
+    test_edt = MetarGrabber::Filepath.gem_filename(Time.new(2007,11,1,12, 5,0, "-05:00"), 'sao')
     test_edt.should == '20071101_sao.gem'
   end
 
-  describe '.surface_filename when UTC is next day' do
-    test_utc = MetarGrabber::Filepath.surface_filename(Time.new(2007,11,1,20, 5,0, "-05:00"))
+  describe '.gem_filename when UTC is next day' do
+    test_utc = MetarGrabber::Filepath.gem_filename(Time.new(2007,11,1,20, 5,0, "-05:00"), 'sao')
     test_utc.should == '20071102_sao.gem'
   end
 
