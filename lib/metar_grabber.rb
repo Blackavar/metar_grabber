d3f2dd41f0d1dd4e34465e10fb1454332e48e942
Michael Myers <blackavr@gmail.com>
2013-07-04 10:58:02 -0700

    surface_filename generates files based on time in UTC form

--- lib/metar_grabber.rb
+++ lib/metar_grabber.rb
@@ -1,7 +1,9 @@
 class MetarGrabber
   module MetarGrabber::Filepath
     def self.surface_filename(input_time)
-
+      input_time.utc
+      date_part = input_time.gmtime.strftime('%Y%m%d')
+      "#{date_part}_sao.gem"
     end
 
   end
--- spec/filename_format_spec.rb
+++ spec/filename_format_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe MetarGrabber::Filepath do
-  before :each do
-    @test_time = Time.new(2002, 10, 31, 2, 2, 2, "+02:00")
+  describe '.surface_filename when UTC is same day' do
+    test_edt = MetarGrabber::Filepath.surface_filename(Time.new(2007,11,1,12, 5,0, "-05:00"))
+    test_edt.should == '20071101_sao.gem'
   end
 
-  describe '.surface_filename' do
-    test_path = MetarGrabber::Filepath.surface_filename(@test_time)
+  describe '.surface_filename when UTC is next day' do
+    test_utc = MetarGrabber::Filepath.surface_filename(Time.new(2007,11,1,20, 5,0, "-05:00"))
+    test_utc.should == '20071102_sao.gem'
   end
 
 end
