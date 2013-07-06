1fb5c17ec6d6fc05881df4a9e5057cf2197000b3
Michael Myers <blackavr@gmail.com>
2013-07-06 10:41:15 -0700

    Generates sat filenames

--- lib/metar_grabber.rb
+++ lib/metar_grabber.rb
@@ -6,5 +6,14 @@ class MetarGrabber
       "#{date_part}#{hour}_#{ext}.gem"
     end
 
+    def self.sat_filename(input_time,sat_type,start=15)
+      input_time.utc
+      date_part = input_time.gmtime.strftime('%Y%m%d')
+      filenames = []
+      filenames[0] = "#{sat_type}_#{date_part}_#{input_time.gmtime.strftime('%H')}#{start}"
+      filenames[1] = "#{sat_type}_#{date_part}_#{input_time.gmtime.strftime('%H')}#{start+30}"
+      filenames
+    end
+
   end
 end
--- spec/filename_format_spec.rb
+++ spec/filename_format_spec.rb
@@ -23,4 +23,11 @@ describe MetarGrabber::Filepath do
     end
   end
 
+  describe '.sat_filename' do
+    # TYP_YYYYMMDD_HHmm - minutes are 30 min apart, on hour/half or 15/45
+    it 'when UTC is same day and time is not ' do
+      test_edt = MetarGrabber::Filepath.sat_filename(Time.new(2007,11,1,12, 5,0, "-05:00"), 'IR')
+      test_edt.should == ['IR_20071101_1715','IR_20071101_1745']
+    end
+  end
 end
