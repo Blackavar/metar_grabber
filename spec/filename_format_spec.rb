146ecf584be7080791a52d1979f675568dfe26c5
Michael Myers <blackavr@gmail.com>
2013-07-06 09:23:26 -0700

    Fix thinko in rspec syntax - counts examples properly

--- spec/filename_format_spec.rb
+++ spec/filename_format_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 
 describe MetarGrabber::Filepath do
-  describe '.gem_filename when UTC is same day' do
-    test_edt = MetarGrabber::Filepath.gem_filename(Time.new(2007,11,1,12, 5,0, "-05:00"), 'sao')
-    test_edt.should == '20071101_sao.gem'
-  end
+  describe '.gem_filename' do
+    it 'when UTC is same day' do
+      test_edt = MetarGrabber::Filepath.gem_filename(Time.new(2007,11,1,12, 5,0, "-05:00"), 'sao')
+      test_edt.should == '20071101_sao.gem'
+    end
 
-  describe '.gem_filename when UTC is next day' do
-    test_utc = MetarGrabber::Filepath.gem_filename(Time.new(2007,11,1,20, 5,0, "-05:00"), 'sao')
-    test_utc.should == '20071102_sao.gem'
-  end
+    it 'when UTC is next day' do
+      test_utc = MetarGrabber::Filepath.gem_filename(Time.new(2007,11,1,20, 5,0, "-05:00"), 'sao')
+      test_utc.should == '20071102_sao.gem'
+    end
 
-  describe '.gem_filename with hour - same day' do
-    test_edt = MetarGrabber::Filepath.gem_filename(Time.new(2007,11,1,12, 5,0, "-05:00"), 'warn', 12)
-    test_edt.should == '2007110112_warn.gem'
-  end
+    it 'with hour - same day' do
+      test_edt = MetarGrabber::Filepath.gem_filename(Time.new(2007,11,1,12, 5,0, "-05:00"), 'warn', 12)
+      test_edt.should == '2007110112_warn.gem'
+    end
 
-  describe '.gem_filename with hour - next day' do
-    test_edt = MetarGrabber::Filepath.gem_filename(Time.new(2007,11,1,20, 5,0, "-05:00"), 'warn', 12)
-    test_edt.should == '2007110212_warn.gem'
+    it 'with hour - next day' do
+      test_edt = MetarGrabber::Filepath.gem_filename(Time.new(2007,11,1,20, 5,0, "-05:00"), 'warn', 12)
+      test_edt.should == '2007110212_warn.gem'
+    end
   end
 
 end
