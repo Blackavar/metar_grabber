4dabc10c3476e569ece8b38f9da2ffa98914937d
Michael Myers <blackavr@gmail.com>
2013-07-17 17:21:41 -0700

    Add RCov formatter for Jenkins test coverage graphing

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "http://rubygems.org"
 
 gem 'configuration'
 gem 'simplecov'
+gem 'simplecov-rcov'
 gem 'guard-rspec'
 gem 'json', '~> 1.7.7'
 gem 'rake'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start
 
 require_relative '../lib/metar_grabber'
